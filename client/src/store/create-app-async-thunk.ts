2bf03897275fe738f346974d2839da178201b380
Oleg Akimov <akimba@gmail.com>
2023-10-25 13:11:59 +0300

    feat: Типизирует thunk creator

---
+++ client/src/store/create-app-async-thunk.ts
@@ -0,0 +1,9 @@
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import { RootState, AppDispatch } from './store';
+import { AxiosInstance } from 'axios';
+
+export const createAppAsyncThunk = createAsyncThunk.withTypes<{
+  state: RootState;
+  dispatch: AppDispatch;
+  extra: AxiosInstance;
+}>();
