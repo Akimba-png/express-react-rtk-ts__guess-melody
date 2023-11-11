72b7eb08a7aed18bab6017ecb049d28b105bb5ce
Oleg Akimov <akimba@gmail.com>
2023-11-11 18:18:43 +0300

    fix: Исправляет тайпинги для withPlayer Hoc

--- client/src/hocks/with-player/with-player.tsx
+++ client/src/hocks/with-player/with-player.tsx
@@ -2,19 +2,20 @@ import { ComponentType, useState } from 'react';
 import { Player } from '../../components/player/player';
 
 type RenderProp = {
-  render: (id: number) => JSX.Element;
+  render: (id: number, src: string) => JSX.Element;
 };
 
 function withPlayer<T>(
-  Component: ComponentType<T & RenderProp>
-): ComponentType<T> {
-  function WithPlayer(props: T) {
+  Component: ComponentType<T>
+): ComponentType<Omit<T, keyof RenderProp>> {
+  function WithPlayer(props: Omit<T, keyof RenderProp>) {
     const [activePlayer, setActivePlayer] = useState(-1);
     return (
       <Component
-        {...props}
-        render={(id: number) => (
+        {...props as T}
+        render={(id: number, src: string) => (
           <Player
+            src={src}
             isPlaying={activePlayer === id}
             onPlayButtonClick={() =>
               setActivePlayer(activePlayer === id ? -1 : id)
