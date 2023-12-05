4c6c819813948418f40ebb4f1b974af23204c003
Oleg Akimov <akimba@gmail.com>
2023-12-05 18:48:11 +0300

    feat: Добавляет логику начала игры

--- client/src/screens/welcome-screen/welcome-screen.tsx
+++ client/src/screens/welcome-screen/welcome-screen.tsx
@@ -1,4 +1,9 @@
+import { useNavigate } from 'react-router-dom';
+import { AppRoute } from '../../constants/const';
+
 function WelcomeScreen(): JSX.Element {
+  const navigate = useNavigate();
+
   return (
     <section className="welcome">
       <div className="welcome__logo">
@@ -9,7 +14,10 @@ function WelcomeScreen(): JSX.Element {
           height="83"
         />
       </div>
-      <button className="welcome__button">
+      <button
+        onClick={() => navigate(AppRoute.Game)}
+        className="welcome__button"
+      >
         <span className="visually-hidden">Начать игру</span>
       </button>
       <h2 className="welcome__rules-title">Правила игры</h2>
