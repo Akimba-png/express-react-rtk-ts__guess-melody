a0d8cd77a7722969c7070c109edd384ae376de36
Oleg Akimov <akimba@gmail.com>
2023-12-06 14:40:05 +0300

    test: Adds Welcom Screen test case

---
+++ client/src/screens/welcome-screen/welcome-screen.test.tsx
@@ -0,0 +1,33 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+// import { WelcomeScreen } from './welcome-screen';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import { AppRoute } from '../../constants/const';
+import { WelcomeScreen } from './welcome-screen';
+
+const router = createMemoryRouter([
+  {
+    path: AppRoute.Main,
+    element: <WelcomeScreen />,
+  },
+  {
+    path: AppRoute.Game,
+    element: <h1>Game screen</h1>,
+  },
+], {
+  initialEntries: [AppRoute.Main],
+  initialIndex: 0,
+});
+
+describe('Component WelcomeScreen', () => {
+  it('should render properly', () => {
+    render(<RouterProvider router={router} />)
+    expect(screen.getByText(/Правила игры/)).toBeInTheDocument();
+  });
+
+  it('should navigate to game screen on play button click', () => {
+    render(<RouterProvider router={router} />);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    expect(screen.getByText(/Game screen/)).toBeInTheDocument();
+  })
+});
