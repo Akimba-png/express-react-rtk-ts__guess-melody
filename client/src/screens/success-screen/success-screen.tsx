7f2f3f0f695117d504b3c9a1b57296775c47e0c5
Oleg Akimov <akimba@gmail.com>
2024-01-07 18:42:42 +0300

    feat: Добавляет логику SuccessScreen

--- client/src/screens/success-screen/success-screen.tsx
+++ client/src/screens/success-screen/success-screen.tsx
@@ -1,10 +1,24 @@
+import { type MouseEvent } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAppSelector, useAppDispatch } from './../../hooks/store-hooks';
+import { resetGame } from '../../store/slices/game-process-slice/game-process-slice';
+import { AppRoute } from '../../constants/const';
+
 function SuccessScreen(): JSX.Element {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { step, errorCount } = useAppSelector((state) => state.gameProcess);
+  const handlePlayAgainClick = (evt: MouseEvent<HTMLButtonElement>) => {
+    evt.preventDefault();
+    dispatch(resetGame());
+    navigate(AppRoute.Game);
+  };
   return (
     <section className="result">
       <div className="result-logout__wrapper">
-        <a className="result-logout__link" href="#">
+        <Link className="result-logout__link" to={AppRoute.Main}>
           Выход
-        </a>
+        </Link>
       </div>
       <div className="result__logo">
         <img
@@ -16,9 +30,13 @@ function SuccessScreen(): JSX.Element {
       </div>
       <h2 className="result__title">Вы настоящий меломан!</h2>
       <p className="result__total">
-        Вы ответили правильно на 6 вопросов и совершили 2 ошибки
+        Вы ответили правильно на {step} вопросов и совершили {errorCount} ошибки
       </p>
-      <button className="replay" type="button">
+      <button
+        className="replay"
+        type="button"
+        onClick={handlePlayAgainClick}
+      >
         Сыграть ещё раз
       </button>
     </section>
