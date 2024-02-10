f79af0eafeb0c079f095e97c3ab1d0ad92df2163
Oleg Akimov <akimba@gmail.com>
2024-02-10 13:40:36 +0300

    feat: Add access token service

---
+++ client/src/services/token-service.ts
@@ -0,0 +1,17 @@
+import { AUTHORIZATION } from '../constants/const';
+
+class TokenService {
+  getToken() {
+    return localStorage.getItem(AUTHORIZATION);
+  }
+
+  setToken(token: string) {
+    localStorage.setItem(AUTHORIZATION, token);
+  }
+
+  resetToken() {
+    localStorage.setItem(AUTHORIZATION, '');
+  }
+}
+
+export const tokenService = new TokenService();
